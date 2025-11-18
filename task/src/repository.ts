0584b61a1f4bbe7598c428963a9bc4e1847ca528
Admin <admin@MngEnvMCAP162297.onmicrosoft.com>
2025-11-18 11:53:27 -0300

    Fix: Use binary-extensions package instead of local JSON file
    Issue: Runtime error 'Cannot find module ./binaryExtensions.json'
    Cause: TypeScript doesn't copy JSON files to dist folder by default
    
    Solution:
    - Changed import from local JSON to npm package 'binary-extensions'
    - Removed local binaryExtensions.json and .d.ts files
    - Updated tsconfig to include allowSyntheticDefaultImports
    
    Benefits:
    - No need to manage local copy of binary extensions
    - Always up-to-date with package updates
    - Cleaner project structure
    - Fixes runtime module resolution error
    
    Tested: Code compiles successfully and dist folder is generated correctly

--- task/src/repository.ts
+++ task/src/repository.ts
@@ -1,6 +1,6 @@
 import * as tl from "azure-pipelines-task-lib/task";
 import { SimpleGit, SimpleGitOptions, simpleGit } from "simple-git";
-import binaryExtensions from "./binaryExtensions.json";
+import binaryExtensions from "binary-extensions";
 import { minimatch } from "minimatch";
 
 export class Repository {
